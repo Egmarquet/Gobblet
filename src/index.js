5bdad4ad894eca5324e391de91ae0ba03f26a4ea
egmarquet <egmarquet@gmail.com>
2020-04-27 11:54:03 -0400

    basic gobblet movement demo

--- src/index.js
+++ src/index.js
@@ -1,44 +1,85 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Konva from 'konva';
-import { Stage, Layer, Group, Circle, Line} from 'react-konva';
+import { Stage, Layer, Circle, Line, Arrow} from 'react-konva';
 
-function BoardState(){
+const BoardManager = {
 
-  const produceGobblet = (player, color, size) => {
-    var gobblet = {player: player, color: color, size:size}
+  produceGobblet(player, color, size){
+    var gobblet = {player: player, color: color, size: size}
     return gobblet
-  }
+  },
 
-  const getInitialBoardState = () => {
+  getInitialBoardState(){
     const emptyBoard = [
         [ [],[],[],[] ],
         [ [],[],[],[] ],
         [ [],[],[],[] ],
         [ [],[],[],[] ]]
-        
     return emptyBoard
-  }
+  },
+
+  sanitizeCoordinates(coord){
+    if (coord <= 0){
+      return 0
+    }
+    else if (coord >= 3){
+      return 3
+    }
+    else{
+      return coord
+    }
+  },
 
   getGobblet(board, x, y){
+    x = this.sanitizeCoordinates(x)
+    y = this.sanitizeCoordinates(y)
     if (board[x][y].length > 0){
       return board[x][y][board[x][y].length-1]
     }
     return null
-  }
+  },
 
   pushGobblet(board, x, y, gobblet){
     var boardStateCopy = Array.from(board)
     boardStateCopy[x][y].push(gobblet)
     return boardStateCopy
-  }
+  },
 
   popGobblet(board, x, y){
     var boardStateCopy = Array.from(board)
     var gobblet = boardStateCopy[x][y].pop()
     return [boardStateCopy, gobblet]
-  }
+  },
 
+  moveIsValid(board, x1, y1, x2, y2){
+    const g1 = this.getGobblet(board, x1, y1)
+    const g2 = this.getGobblet(board, x2, y2)
+    if (g1 === null){
+        return false
+    }
+    else if (g2 === null){
+      return true
+    }
+    else{
+      if (g1.size > g2.size){
+        return true
+      }
+    }
+    return false
+  },
+
+  move(board, x1, y1, x2, y2){
+    if (this.moveIsValid(board, x1, y1, x2, y2)){
+      var [board, gobblet] = this.popGobblet(board, x1, y1)
+      board = this.pushGobblet(board, x2, y2, gobblet)
+      return board
+    }
+    else{
+      return board
+    }
+  }
+}
 
 function Board( {width, height, color, boardState} ){
 
@@ -52,10 +93,24 @@ function Board( {width, height, color, boardState} ){
     [0,3*height/4, width,3*height/4],
   ]
 
-  const gobblets = boardState.map( ( row, i ) => { return (
+  const squareToCenterCoord = (x, y) => {
+    return [width/8 + x*width/4, height/8 + y*height/4]
+  }
+
+  const renderGobblets = boardState.map( ( row, i ) => { return (
     row.map( (square, j) => {
       if(square.length > 0){
-        return(square[square.length-1])
+        var gobblet = square[square.length-1]
+        return(
+          <Circle
+            x = {squareToCenterCoord(i,j)[0]}
+            y = {squareToCenterCoord(i,j)[1]}
+            fill = {gobblet.color}
+            stroke = {"black"}
+            strokeWidth = {1}
+            radius = {gobblet.size}
+          />
+        )
       }
       return(null)
     })
@@ -75,17 +130,9 @@ function Board( {width, height, color, boardState} ){
   return(
     <Layer>
       {borders}
+      {renderGobblets}
     </Layer>
   );
-}
-
-function Arrow({x1, y1, x2, y2, selectedGobblet, x2Center, y2Center}){
-  return(
-    <Line
-      points = {[x1, y1, x2, y2]}
-      stroke = {"black"}
-    />
-  )
 }
 
 /*
@@ -97,21 +144,14 @@ function Arrow({x1, y1, x2, y2, selectedGobblet, x2Center, y2Center}){
 function Game(){
   const width = 500;
   const height = 500;
-  const offsetX = 0;
-  const offsetY = 0;
-  const emptyBoard = [
-      [ [],[],[],[] ],
-      [ [],[],[],[] ],
-      [ [],[],[],[] ],
-      [ [],[],[],[] ]]
 
   // Enums
   const gameStates = {WAIT:0, PLACE:1, OPPTURN: 2}
   const GOBBLETSIZE = {SMALL:10, MEDIUM:30, LARGE:50}
-  const board = new BoardState(emptyBoard)
 
   //Overall gamestate tracking
-  const [boardState, setBoardState] = useState(board.getState())
+  const boardManager = BoardManager
+  const [boardState, setBoardState] = useState(boardManager.getInitialBoardState())
   const [playerState, setPlayerState] = useState(gameStates.WAIT)
 
   // Mouse tracking
@@ -119,11 +159,10 @@ function Game(){
   const [mousedOverSquare, setMousedOverSquare] = useState([0,0])
   const [isDragging, setIsDragging] = useState(false)
   const [selectedSquare, setSelectedSquare] = useState([0,0])
-  const [selectedGobblet, setSelectedGobblet] = useState(null)
 
   const mouseTracker = (e) => {
-    const x = parseInt(e.evt.offsetX,10)
-    const y = parseInt(e.evt.offsetY,10)
+    var x = parseInt(e.evt.offsetX,10)
+    var y = parseInt(e.evt.offsetY,10)
     // bounding the mouse to play area
     if (x < 0){
       x = 0
@@ -147,20 +186,21 @@ function Game(){
 
   // Main game interaction logic
   const handleDrag = (e) => {
-    const square = [Math.floor(mousePosition[0]/(width/4)), Math.floor(mousePosition[1]/(height/4))]
     if (!isDragging){
-      var coordX = Math.floor(mousePosition[0]/(width/4))
-      var coordY = Math.floor(mousePosition[1]/(height/4))
-
+      var x = Math.floor(mousePosition[0]/(width/4))
+      var y = Math.floor(mousePosition[1]/(height/4))
       // If valid moveable gobblet exists at that position
-      if (boardState[coordX][coordY].length > 0){
-        setSelectedSquare([coordX, coordY])
+      if (boardManager.getGobblet(boardState, x, y) != null){
+        setSelectedSquare([x, y])
         setIsDragging(true)
       }
     }
   }
 
   const handleRelease = (e) => {
+    if (boardManager.moveIsValid(boardState, selectedSquare[0], selectedSquare[1], mousedOverSquare[0], mousedOverSquare[1])){
+      setBoardState(boardManager.move(boardState,selectedSquare[0], selectedSquare[1], mousedOverSquare[0], mousedOverSquare[1]))
+    }
     setSelectedSquare([0,0])
     setIsDragging(false)
   }
@@ -170,38 +210,69 @@ function Game(){
   // ------------------------------------- //
 
   const randomGenGobblets = () => {
-    for (var i = 0; i<5; i++){
+    var boardStateCopy = Array.from(boardManager.getInitialBoardState());
+    const sizes = [10, 30, 50]
+    for (var i = 0; i<7; i++){
       var size = Math.floor(Math.random()*3)
       var x = Math.floor(Math.random()*4)
       var y = Math.floor(Math.random()*4)
-      var gobblet = board.produceGobblet(0, "#3287a8", 50)
-
-      board.pushGobblet(x, y, setBoardState)
+      var gobblet = boardManager.produceGobblet(0, "#3287a8", sizes[size])
+      boardStateCopy = boardManager.pushGobblet(boardStateCopy, x, y, gobblet)
     }
+    setBoardState(boardStateCopy)
   }
 
-  /*
-  const ghostGobblet = () => {
-    if (isDragging && !squareIsOccupied(mousedOverSquare[0], mousedOverSquare[1])){
-      const refGobblet = getGobblet(selectedSquare[0],selectedSquare[1])
-      if (refGobblet){
-        return(
-          <Circle
-            x = {squareToCenterCoord(mousedOverSquare[0],mousedOverSquare[1])[0]}
-            y = {squareToCenterCoord(mousedOverSquare[0],mousedOverSquare[1])[1]}
-            radius = {refGobblet.props.radius}
-            fill = {refGobblet.props.fill}
-            dash = {[5,1]}
-            stroke = {"black"}
-            opacity = {0.5}
-          />
-        )
+  const placementArrow = () => {
+    if (isDragging){
+      const refGobblet = boardManager.getGobblet(boardState,selectedSquare[0],selectedSquare[1])
+      if (refGobblet ){
+        if (boardManager.moveIsValid(boardState, selectedSquare[0], selectedSquare[1], mousedOverSquare[0], mousedOverSquare[1])){
+          return(
+            <Layer>
+              <Circle
+                x = {squareToCenterCoord(mousedOverSquare[0],mousedOverSquare[1])[0]}
+                y = {squareToCenterCoord(mousedOverSquare[0],mousedOverSquare[1])[1]}
+                radius = {refGobblet.size}
+                fill = {refGobblet.color}
+                dash = {[5,1]}
+                stroke = {"black"}
+                opacity = {0.5}
+              />
+              <Arrow
+                stroke = {"black"}
+                points = {[
+                  squareToCenterCoord(selectedSquare[0],selectedSquare[1])[0],
+                  squareToCenterCoord(selectedSquare[0],selectedSquare[1])[1],
+                  mousePosition[0],
+                  mousePosition[1]
+                ]}
+                strokeWidth = {1}
+              />
+            </Layer>
+          )
+        }
+        else{
+          return(
+            <Layer>
+              <Arrow
+                stroke = {"black"}
+                points = {[
+                  squareToCenterCoord(selectedSquare[0],selectedSquare[1])[0],
+                  squareToCenterCoord(selectedSquare[0],selectedSquare[1])[1],
+                  mousePosition[0],
+                  mousePosition[1]
+                ]}
+                strokeWidth = {1}
+              />
+            </Layer>
+          )
+        }
       }
     }
 
     return null
   }
-  */
+
 
   return(
     <div>
@@ -218,24 +289,11 @@ function Game(){
           color = {"#3287a8"}
           boardState = {boardState}
         />
-
-        <Layer>
-          {isDragging
-            ?
-            <Arrow
-              x1= {squareToCenterCoord(selectedSquare[0],selectedSquare[1])[0]}
-              y1= {squareToCenterCoord(selectedSquare[0],selectedSquare[1])[1]}
-              x2= {mousePosition[0]}
-              y2= {mousePosition[1]}
-            />
-            : null
-          }
-        </Layer>
+        {placementArrow()}
       </Stage>
-
       <button onClick={randomGenGobblets}>Random gobblets</button>
     </div>
-  );
+  )
 }
 
 ReactDOM.render(
